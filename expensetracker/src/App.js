5b615501f22df23486d759b00d229a11ebfee127
sushilkumar666 <sushilssharma064@gmail.com>
2023-11-15 11:50:57 +0530

    react_first_component

---
+++ expensetracker/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import ExpenseTracker from './Components/ExpenseTracker'
+
+function App() {
+  return (
+   <ExpenseTracker></ExpenseTracker>
+  );
+}
+
+export default App;
---
+++ expensetracker/src/Components/ExpenseTracker.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function ExpenseTracker() {
+  return (
+    <>    <h1>ExpenseTracker</h1>
+     <p>
+    <h2> Expense Items </h2>
+
+Food Rs 10 <br></br>
+
+Petrol Rs 100 <br></br>
+
+Movies Rs 200
+
+     </p>
+    </>
+
+  )
+}
