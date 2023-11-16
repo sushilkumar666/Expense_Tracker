9ee91b84e0aaac670e81aee633c74cc93787fca9
Sushilkumar Sharma <73883613+sushilkumar666@users.noreply.github.com>
2023-11-16 23:12:35 +0530

    click function added

--- expensetracker/src/Components/Expenses/ExpenseItem.js
+++ expensetracker/src/Components/Expenses/ExpenseItem.js
@@ -3,10 +3,16 @@ import './ExpenseItem.css'
 import ExpenseDetail from './ExpenseDetail'
 import ExpenseDate from './ExpenseDate'
 function ExpenseItem(props) {
+  let clickMe = () => {
+    console.log('clicked Me');
+  }
   return (
-    <div className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <ExpenseDetail amount={props.amount} location={props.location} title={props.title} />
+    <div style={{ width: '60%', textAlign: 'center', marginLeft: 'auto', marginRight: 'auto', backgroundColor: 'black', padding: '10px' }}>
+      <div className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <ExpenseDetail amount={props.amount} location={props.location} title={props.title} />
+        <buton style={{ backgroundColor: 'white', cursor: 'pointer' }} onClick={clickMe}>Click Me</buton>
+      </div>
     </div>
   );
 }
