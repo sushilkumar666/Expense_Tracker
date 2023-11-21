167b5bd210ba728c1d446c805862ce33115b78c0
sushilkumar sharma <sushilsharma064@gmail.com>
2023-11-22 00:08:24 +0530

    add 100 on buttonclick

--- expensetracker/src/Components/Expenses/ExpenseItem.js
+++ expensetracker/src/Components/Expenses/ExpenseItem.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseItem.css'
 import ExpenseDetail from './ExpenseDetail'
 import ExpenseDate from './ExpenseDate'
 function ExpenseItem(props) {
+  const [amount, setAmount] = useState(props.amount);
   let clickMe = () => {
-    console.log('clicked Me');
+    
+    setAmount(+amount + 100);
   }
   return (
     <div style={{ width: '60%', textAlign: 'center', marginLeft: 'auto', marginRight: 'auto', backgroundColor: 'black', padding: '10px' }}>
       <div className='expense-item'>
         <ExpenseDate date={props.date} />
-        <ExpenseDetail amount={props.amount} location={props.location} title={props.title} />
-        <buton style={{ backgroundColor: 'white', cursor: 'pointer' }} onClick={clickMe}>Click Me</buton>
+        <ExpenseDetail amount={amount} location={props.location} title={props.title} />
+        <button style={{ backgroundColor: 'white', cursor: 'pointer', padding: '10px', borderRadius:'10px' ,marginLeft:'5px' }} onClick={clickMe}>Click Me</button>
       </div>
     </div>
   );
