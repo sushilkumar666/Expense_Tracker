86a5c643475d2af32446c577813e7309ce7bbffd
sushilkumar sharma <sushilsharma064@gmail.com>
2023-11-22 01:18:49 +0530

    create seperate call for each form element

--- expensetracker/src/Components/ExpenseForm.js
+++ expensetracker/src/Components/ExpenseForm.js
@@ -1,23 +1,35 @@
 import {useState} from 'react'
 
 const ExpenseForm =()=> {
-    const[title, setTitle] = useState('');
-    const[amount, setAmount] = useState('');
+    const[enteredtitle , setEnteredtitle ] = useState('');
+    const[enteredAmount , setEnteredAmount ] = useState('');
 
-    const[Date, setDate] = useState('');
+    const[enteredDate , setEnteredDate ] = useState('');
 
 
-    const change = (e) =>{
+    const onTitleChange = (e) =>{
         console.log(e.target.value);
     }
+
+    const onAmountChange = (e) =>{
+        console.log(e.target.value);
+    }
+
+    const onDateChange = (e) =>{
+        console.log(e.target.value);
+    }
+
+
+
     return(
         <form >
             <label htmlFor="title">Expense Title</label>
-            <input id="title" onChange={change} /> <br />
+            <input id="title" onChange={onTitleChange} /> <br />
             <label htmlFor="amount">Expense Amount</label>
-            <input id="amount"  onChange={change} /> <br />
-            <label htmlFor="amount">Date</label>
-            <input type="date"  onChange={change} id="amount" />
+            <input id="amount"  onChange={onAmountChange} /> <br />
+            <label htmlFor="date">Date</label>
+            <input type="date" min='2019-01-01' max='2023-12-31'  onChange={onDateChange} id="date" /> <br />
+            <button type='submit'>Add</button>
            
 
         </form>
