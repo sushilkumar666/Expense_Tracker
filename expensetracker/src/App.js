395b1d11486dd7783b572ef3af06c07cd5e2f2e0
sushilkumar666 <sushilssharma064@gmail.com>
2023-11-22 11:38:25 +0530

    testing

--- expensetracker/src/App.js
+++ expensetracker/src/App.js
@@ -355,7 +355,7 @@ function App() {
 
 
       {data.map((element) => (
-        <ExpenseItem title={element.title} date={element.date} location={element.location} amount={element.amount}></ExpenseItem >
+        <ExpenseItem title={element.title} date={element.date} location={element.location} amount={Math.floor(Math.random() * (5000 - 1000) + 1000)}></ExpenseItem >
       ))}
 
 
