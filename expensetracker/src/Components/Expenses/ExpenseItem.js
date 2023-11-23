b0015c8c941ff326277fcdfe78d0e7f8b1426aaa
Sushilkumar Sharma <73883613+sushilkumar666@users.noreply.github.com>
2023-11-23 22:25:30 +0530

    added filter

--- expensetracker/src/Components/Expenses/ExpenseItem.js
+++ expensetracker/src/Components/Expenses/ExpenseItem.js
@@ -3,15 +3,15 @@ import './ExpenseItem.css'
 import ExpenseDetail from './ExpenseDetail'
 import ExpenseDate from './ExpenseDate'
 function ExpenseItem(props) {
-  let clickMe = () => {
-    console.log('clicked Me');
+  let clickMe = (e) => {
+    e.target.parentElement.remove();
   }
   return (
     <div style={{ width: '60%', textAlign: 'center', marginLeft: 'auto', marginRight: 'auto', backgroundColor: 'black', padding: '10px' }}>
       <div className='expense-item'>
         <ExpenseDate date={props.date} />
         <ExpenseDetail amount={props.amount} location={props.location} title={props.title} />
-        <buton style={{ backgroundColor: 'white', cursor: 'pointer' }} onClick={clickMe}>Click Me</buton>
+        <buton style={{ backgroundColor: 'white', cursor: 'pointer' }} onClick={clickMe}>Delete</buton>
       </div>
     </div>
   );
